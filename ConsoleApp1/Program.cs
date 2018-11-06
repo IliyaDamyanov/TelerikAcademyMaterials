a5e19343b5dcd4cec2147f0f6244b5ed1530a685
IliyaDamyanov <i.s.damyanov@gmail.com>
2018-11-06 23:36:40 +0200

    Initial Commit
    Initial Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(@"C:\Users\IliyaDamyanov\Desktop\Links.docx", true))
+            {
+                using (StreamReader sr = new StreamReader(wordDoc.MainDocumentPart.GetStream()))
+                {
+                    string docText = sr.ReadToEnd();
+                    using (StreamWriter streamWriter = new StreamWriter("txt.txt"))
+                    {
+                        streamWriter.Write(docText);
+                    }
+                }
+            }
+        }
+    }
+}
